b7fca566811f4ca959cf2c121029c3b2c9c0f72a
vportasaturno-cpu <valenportacba@gmail.com>
2025-12-04 10:40:32 -0300

    algunas correcciones

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from typing import Optional
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 load_dotenv()
 
